9cccfe78a104fc99e96ea8a7c638de6c828ced29
Yabe.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2021-03-06 21:35:15 +0900

    Remove unnecessary comments

--- aarch64-buildenv/activate.sh
+++ aarch64-buildenv/activate.sh
@@ -85,6 +85,7 @@ ORIG_PATH=${PATH}
 export PATH=${ORIG_PATH}:${SCRIPT_DIR}/osbook/devenv/
 export MIKANOS_COMPILEDB=${SCRIPT_DIR}/mikanos/compile_commands.json
 export QEMU_LIBPATH="-L ${SCRIPT_DIR}/seabios"
+#-L /usr/share/qemu -L /usr/share/seabios"
 
 alias edk="cd ${SCRIPT_DIR}/edk2"
 alias osb="cd ${SCRIPT_DIR}/osbook"
