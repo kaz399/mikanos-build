198f9eef243804dc9a6b5619004365b5faa11b78
Yabe.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2021-03-08 15:53:16 +0900

    Refactor build scripts (x86_64)

--- devenv/buildenv-aarch64.sh
+++ devenv/buildenv-aarch64.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 export TARGET_ARCH=aarch64
-export SYSROOT="${AARCH64_GCC_PATH}"
 export DEVENV_DIR="${SCRIPT_DIR}"
+export SYSROOT="${AARCH64_GCC_PATH}"
 
--- devenv/buildenv-x86_64.sh
+++ devenv/buildenv-x86_64.sh
@@ -3,13 +3,6 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 export TARGET_ARCH=x86_64
-export MIKANOS_ARCH=ARCH_X86_64
-export MIKANOS_TARGET_LIBDIR=x86_64-elf
-export CLANG_TARGET=x86_64-linux-gnu
+export DEVENV_DIR="${SCRIPT_DIR}"
 export SYSROOT=
 
-BASEDIR="${SCRIPT_DIR}/${MIKANOS_TARGET_LIBDIR}"
-CROSSBUILD_FLAGS=
-
-export CPPFLAGS="${CROSSBUILD_FLAGS} -I${BASEDIR}/include/c++/v1 -I${BASEDIR}/include -I${BASEDIR}/include/sys -I${BASEDIR}/include/freetype2 -nostdlibinc -D__ELF__ -D_LDBL_EQ_DBL -D_GNU_SOURCE -D_POSIX_TIMERS -D${MIKANOS_ARCH}=1"
-export LDFLAGS="-L${BASEDIR}/lib"
