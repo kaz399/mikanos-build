21c54330db68a7dead4ffb2f6f6d756d2d03bd61
Yabe.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2021-03-09 17:14:43 +0900

    Update bootstrap.sh

--- aarch64-buildenv/bootstrap.sh
+++ aarch64-buildenv/bootstrap.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -eu
 
-WORKDIR="$(pwd)"
+if git rev-parse 2> /dev/null ; then
+    REPOTOP="$(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)"
+    WORKDIR="$(cd ${REPOTOP}/../ && pwd)"
+    echo "Inside of a git repository"
+    echo "Go to ${WORKDIR}"
+    cd "${WORKDIR}"
+else
+    echo "Outside of a git repository"
+    WORKDIR="$(pwd)"
+fi
 
 TOOLCHAIN_DIR=${1:-${HOME}/.mikanos.toolchain}
 DOWNLOAD_DIR=${2:-"${WORKDIR}"/downloads}
-
-if [[ -s ${HOME}/.mikanos.toolchain_path ]] ; then
-    . ${HOME}/.mikanos.toolchain_path
-fi
 
 # get source code and binaries
 
@@ -58,7 +63,7 @@ fi
 
 # build environment settings
 
-DEVENV_DIR="${WORKDIR}"/osbook/devenv/aarch64-linux-gnu
+DEVENV_DIR="${WORKDIR}/osbook/devenv/aarch64-linux-gnu"
 CPUS=$(nproc --ignore=2)
 
 mkdir -p "${TOOLCHAIN_DIR}"
@@ -71,45 +76,45 @@ mkdir -p "${DEVENV_DIR}"
 # expand archives
 
 if [[ ! -d "clang+llvm-11.0.0-aarch64-linux-gnu" ]] ; then
-    tar xvf "${DOWNLOAD_DIR}"/clang+llvm-11.0.0-aarch64-linux-gnu.tar.xz
+    tar xvf "${DOWNLOAD_DIR}/clang+llvm-11.0.0-aarch64-linux-gnu.tar.xz" -C "${WORKDIR}"
 fi
-cd "${WORKDIR}"/clang+llvm-11.0.0-aarch64-linux-gnu
+cd "${WORKDIR}/clang+llvm-11.0.0-aarch64-linux-gnu"
 cp -vr * "${DEVENV_DIR}"
-cd "${WORKDIR}"
 
-if [[ ! -d "${TOOLCHAIN_DIR}/aarch64-linux-gnu" ]] ; then
+if [[ ! -d "${WORKDIR}/aarch64-linux-gnu" ]] ; then
     if [[ ! -d "gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu" ]] ; then
-        tar xvf "${DOWNLOAD_DIR}"/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar.xz -C "${TOOLCHAIN_DIR}"
-        cd  "${TOOLCHAIN_DIR}"
+        tar xvf "${DOWNLOAD_DIR}/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar.xz" -C "${WORKDIR}"
+        cd "${WORKDIR}"
         ln -s gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu aarch64-linux-gnu
-        cd aarch64-linux-gnu-gcc/bin/
+        cd "${WORKDIR}/aarch64-linux-gnu/bin/"
         for file in aarch64-none-linux-gnu-*; do ln -s $file $(echo $file | sed -e 's/-none//g'); done
         cd "${WORKDIR}"
     fi
 fi
-cp -v "${TOOLCHAIN_DIR}/aarch64-linux-gnu/lib/gcc/aarch64-none-linux-gnu/10.2.1/libgcc.a" "${DEVENV_DIR}/lib/libgcc.a"
+cp -v "${WORKDIR}/aarch64-linux-gnu/lib/gcc/aarch64-none-linux-gnu/10.2.1/libgcc.a" "${DEVENV_DIR}/lib/libgcc.a"
 
 if [[ ! -d "${WORKDIR}/osbook/devenv/x86_64-elf" ]] ; then
     tar xvf "${DOWNLOAD_DIR}/x86_64-elf.tar.gz" -C "${WORKDIR}"/osbook/devenv
 fi
-echo "export PATH=\"${TOOLCHAIN_DIR}/aarch64-linux-gnu/bin:\${PATH}\"" > ${HOME}/.mikanos.toolchain_path
 
-. ${HOME}/.mikanos.toolchain_path
+# add aarch64 gcc to PATH
+export PATH="${WORKDIR}/aarch64-linux-gnu/bin:${PATH}"
 
-# build
+
+# build aarch64 libraries
 
 # musl
 
 #rm -fr musl-1.2.2
 #tar xvf "${DOWNLOAD_DIR}"/musl-1.2.2.tar.gz
-cd "${WORKDIR}"/musl
+cd "${WORKDIR}/musl"
 git clean -dfx
 ./configure --target=aarch64-linux-gnu --disable-shared --prefix="${DEVENV_DIR}"
 make -j${CPUS} install
 
 # freetype2
 
-cd "${WORKDIR}"/freetype2
+cd "${WORKDIR}/freetype2"
 git clean -dfx
 ./autogen.sh
 ./configure --target=aarch64-linux-gnu --disable-shared --prefix="${DEVENV_DIR}"
@@ -117,39 +122,43 @@ make -j${CPUS} install
 
 # edk2
 
-cd "${WORKDIR}"/edk2
+cd "${WORKDIR}/edk2"
+if [ -n "${WORKSPACE}" ] ; then
+    unset WORKSPACE
+fi
 git clean -dfx
 git submodule init
 git submodule update
-cd "${WORKDIR}"/edk2/BaseTools
+cd "${WORKDIR}/edk2/BaseTools"
 make
-cd "${WORKDIR}"/edk2
+cd "${WORKDIR}/edk2"
 set +u
-. "${WORKDIR}"/edk2/edksetup.sh
+. "${WORKDIR}/edk2/edksetup.sh"
 set -u
 #BUILD_COMPILER=GCC5
 BUILD_COMPILER=CLANG38
 export ${BUILD_COMPILER}_AARCH64_PREFIX=aarch64-linux-gnu-
 build -a AARCH64 -t ${BUILD_COMPILER} -p ArmVirtPkg/ArmVirtQemu.dsc
 dd if=/dev/zero of="${DEVENV_DIR}/OVMF_CODE.fd" bs=1M count=64
-dd if="${WORKDIR}"/edk2/Build/ArmVirtQemu-AARCH64/DEBUG_${BUILD_COMPILER}/FV/QEMU_EFI.fd of="${DEVENV_DIR}/OVMF_CODE.fd" conv=notrunc
+dd if="${WORKDIR}/edk2/Build/ArmVirtQemu-AARCH64/DEBUG_${BUILD_COMPILER}/FV/QEMU_EFI.fd" of="${DEVENV_DIR}/OVMF_CODE.fd" conv=notrunc
 dd if=/dev/zero of="${DEVENV_DIR}/OVMF_VARS.fd" bs=1M count=64
-dd if="${WORKDIR}"/edk2//Build/ArmVirtQemu-AARCH64/DEBUG_${BUILD_COMPILER}/FV/QEMU_VARS.fd of="${DEVENV_DIR}/OVMF_VARS.fd" conv=notrunc
+dd if="${WORKDIR}/edk2//Build/ArmVirtQemu-AARCH64/DEBUG_${BUILD_COMPILER}/FV/QEMU_VARS.fd" of="${DEVENV_DIR}/OVMF_VARS.fd" conv=notrunc
 
 ln -s ../mikanos/MikanLoaderPkg .
 
 # seabios
 
-cd "${WORKDIR}"/seabios
+cd "${WORKDIR}/seabios"
 git clean -dfx
-cp "${WORKDIR}"/osbook/aarch64-buildenv/seabios.config "${WORKDIR}"/seabios/.config
+cp "${WORKDIR}/osbook/aarch64-buildenv/seabios.config" "${WORKDIR}/seabios/.config"
 make
-cp "${WORKDIR}"/seabios/out/vgabios.bin "${WORKDIR}"/seabios/vgabios-ramfb.bin
+cp "${WORKDIR}/seabios/out/vgabios.bin" "${WORKDIR}/seabios/vgabios-ramfb.bin"
 
 
 # copy activate.sh
 
-cp "${WORKDIR}"/osbook/aarch64-buildenv/activate.sh "${WORKDIR}"
+cp "${WORKDIR}/osbook/aarch64-buildenv/activate.sh" "${WORKDIR}"
 
-echo "complete"
+echo ""
+echo "*** complete"
 
